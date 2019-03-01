93f0c009f6129fc2aaa02f6a01d2dee2b795ac92
kalyanchavali <kalyanchavali@gmail.com>
2019-03-01 11:39:09 +0100

    add a loop

--- src/main/java/first/boot/firstboot/HelloWorldService.java
+++ src/main/java/first/boot/firstboot/HelloWorldService.java
@@ -1,15 +1,26 @@
 package first.boot.firstboot;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/rest")
 public class HelloWorldService {
+	static List<Integer> intList = new ArrayList<Integer>();
 
 	@RequestMapping("/hello")
 	public String helloWorld() {
-
+		System.out.println("Invoked Hello world service");
+		madLoop();
 		return "Hello World";
+	}
+
+	private static void madLoop() {
+		for (int i = 0; i < Integer.MAX_VALUE; i++) {
+			intList.add(i);
+		}
 	}
 }
